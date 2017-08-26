90b2c7bf5ea45f02b0f513d9a284ec31a86599f9
njase <e.samehta@gmail.com>
2017-08-26 22:03:59 +0530

    Some refactoring

--- app/mod_rstp/controllers.py
+++ app/mod_rstp/controllers.py
@@ -20,7 +20,6 @@ mod_rstp = Blueprint('rstp', __name__, static_folder='static', url_prefix='/rstp
 def home():
    form = InputForm()
    if request.method == 'GET':
-       print("saurabh : 3")
        return render_template('rstp/rwa_input.html', form = form)
    else:
        print("Unexpected request methof received and ignored")
@@ -30,19 +29,32 @@ def home():
 def input():
    form = InputForm()
    if form.validate() == False:
-       print("saurabh : 1")
        flash('All fields are required.')
        return render_template('rstp/rwa_input.html', form = form)
    else:
-        print("saurabh : 2")
         ##Here, store values in DB and then redirect as GET
         return redirect(url_for('rstp.output',messages=1))
 
 
 @mod_rstp.route('/check/', methods=['GET'])
 def check():
-    #ret_data = {"value": request.args.get('echoValue')}
-    ret_data = {"value": 1}
+    D = "Noimage"
+    P = D
+    U = D
+    V = D
+    #Read the DB index from GET request
+    db_id = 1
+    #Read the status of this simulation from DB
+    #0 = Ongoing, 1 = Finished success , 2 = Stopped, otherwise = internal error
+    sim_status = 1
+    #Make return data
+    if sim_status == 1:
+        D = "/static/"+str(db_id)+"_D_img.jpeg"
+        P = "/static/"+str(db_id)+"_P_img.jpeg"
+        U = "/static/"+str(db_id)+"_U_img.jpeg"
+        V = "/static/"+str(db_id)+"_V_img.jpeg"
+    
+    ret_data = {"sim_status":sim_status,"D":D,"P":P,"U":U,"V":U}
     return jsonify(ret_data)
 
       
@@ -51,13 +63,10 @@ def output():
    form = OutputForm()
    if request.method == 'POST':
       if form.validate() == False:
-         print("saurabh : 5")
          flash('All fields are required.')
          return render_template("rstp/rwa_output.html", form=form)
       else:
-         print("saurabh : 6")
          flash('Stopping of simulation is not supported yet!')
    elif request.method == 'GET':
-         print("saurabh : 7")
          ##Now here read from DB and render output values
          return render_template('rstp/rwa_output.html', form = form)
--- app/mod_rstp/models.py
+++ app/mod_rstp/models.py
@@ -9,32 +9,29 @@ class Base(db.Model):
 
     id            = db.Column(db.Integer, primary_key=True)
     date_created  = db.Column(db.DateTime,  default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime,  default=db.func.current_timestamp(),
-                                           onupdate=db.func.current_timestamp())
 
 # Define a User model
 class User(Base):
 
-    __tablename__ = 'auth_user'
+    __tablename__ = 'UserSimRecord'
 
-    # User Name
-    name    = db.Column(db.String(128),  nullable=False)
-
-    # Identification Data: email & password
-    email    = db.Column(db.String(128),  nullable=False,
-                                            unique=True)
-    password = db.Column(db.String(192),  nullable=False)
-
-    # Authorisation Data: role & status
-    role     = db.Column(db.SmallInteger, nullable=False)
-    status   = db.Column(db.SmallInteger, nullable=False)
+    sim_status    = db.Column(db.SmallInteger)
+    solver        = db.Column(db.Integer)
+    discontinuity = db.Column(db.Float)
+    Vx_left       = db.Column(db.Float)
+    Vx_right      = db.Column(db.Float)
+    Mx_left       = db.Column(db.Float)
+    Mx_right      = db.Column(db.Float)
+    D_left        = db.Column(db.Float)
+    D_right       = db.Column(db.Float)
+    rho_left      = db.Column(db.Float)
+    rho_right     = db.Column(db.Float)
+    cfl           = db.Column(db.Float, default=1.0)
+    mesh_size     = db.Column(db.Integer)
 
     # New instance instantiation procedure
-    def __init__(self, name, email, password):
-
-        self.name     = name
-        self.email    = email
-        self.password = password
+    def __init__(self):
+        pass
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<User %d>' % (self.id)
