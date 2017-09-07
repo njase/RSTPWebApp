a8515c6674f048cc07c4d4fc0c7f569c8f875d58
saurabh <e.samehta@gmail.com>
2017-09-07 12:20:49 +0000

    Phase1 and 2 complete (#3)

--- app/mod_rstp/forms.py
+++ app/mod_rstp/forms.py
@@ -8,21 +8,21 @@ from wtforms import IntegerField, DecimalField, SubmitField, SelectField
 from wtforms import validators, ValidationError
 
 class InputForm(Form):
-   ode_solver = SelectField('Solver method', choices = [('EE', 'Explicit Euler'), ('IE', 'Implicit Euler')])
-   discontinuity = DecimalField("Discontinuity location (%)",[validators.Required("Please enter the location of discontinuity")])
-   Vx_left = DecimalField("",[validators.InputRequired("Please enter the initial value for Vx_left")])
-   Vx_right = DecimalField("",[validators.InputRequired("Please enter the initial value for Vx_right")])
-   Mx_left = DecimalField("",[validators.InputRequired("Please enter the initial value for Mx_left")])
-   Mx_right = DecimalField("",[validators.InputRequired("Please enter the initial value for Mx_right")])
-   D_left = DecimalField("",[validators.InputRequired("Please enter the initial value for D_left")])
-   D_right = DecimalField("",[validators.InputRequired("Please enter the initial value for D_right")])
-   Rho_left = DecimalField("",[validators.InputRequired("Please enter the initial value for Rho_left")])
-   Rho_right = DecimalField("",[validators.InputRequired("Please enter the initial value for Rho_right")])
-   cfl = DecimalField("CFL (only for explicit method)")
-   mesh_size = IntegerField("Number of FV cells",[validators.Required("Please enter the number of FV cells")])
-   gamma = DecimalField("Gamma (0 for isothermal case)",[validators.InputRequired("Please enter the value of gamma")])
-
-   start_sim = SubmitField("Start Simulation")
+    ode_solver = SelectField('Solver method', choices = [('EE', 'Forward Euler'), ('IE', 'Backward Euler'),('CN', 'Crank Nicholson')])
+    discontinuity = DecimalField("Discontinuity location (%)",[validators.Required("Please enter the location of discontinuity")])
+    Vx_left = DecimalField("",[validators.InputRequired("Please enter the initial value for Vx_left")])
+    Vx_right = DecimalField("",[validators.InputRequired("Please enter the initial value for Vx_right")])
+    Mx_left = DecimalField("",[validators.InputRequired("Please enter the initial value for Mx_left")])
+    Mx_right = DecimalField("",[validators.InputRequired("Please enter the initial value for Mx_right")])
+    D_left = DecimalField("",[validators.InputRequired("Please enter the initial value for D_left")])
+    D_right = DecimalField("",[validators.InputRequired("Please enter the initial value for D_right")])
+    Rho_left = DecimalField("",[validators.InputRequired("Please enter the initial value for Rho_left")])
+    Rho_right = DecimalField("",[validators.InputRequired("Please enter the initial value for Rho_right")])
+    cfl = DecimalField("CFL (only for explicit method)")
+    mesh_size = IntegerField("Number of FV cells",[validators.Required("Please enter the number of FV cells")])
+    gamma = SelectField('Gamma', choices = [('0', 'Isothermal'), ('4/3', '4/3'),('5/3', '5/3'),('2', '2')])
+    
+    start_sim = SubmitField("Start Simulation")
 
 class OutputForm(Form):
-   stop_sim = SubmitField("Stop Simulation")
+    stop_sim = SubmitField("Stop Simulation")
